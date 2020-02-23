265dd55676f7931b3ccd6e013a3c4bf82ca4d0c5
cgkennedy <curt.kennedy@kellogg.northwestern.edu>
2020-02-23 20:08:05 +0000

    Project 3

--- app.rb
+++ app.rb
@@ -13,14 +13,19 @@ get "/" do
     @entry = false
     view "ask"
 end
+
 get "/news" do
     results = Geocoder.search(params["q"])
+
     @lat_long = results.first.coordinates # => [lat, long]
+
     @forecast = ForecastIO.forecast(@lat_long[0],@lat_long[1]).to_hash
+
     url = "https://newsapi.org/v2/top-headlines?country=#{results.first.country_code}&apiKey=b96fe19a316d4179b9bb5f5a48073090"
+
     @news = HTTParty.get(url).parsed_response.to_hash
     
-    
     @entry = true
+
     view "ask"
 end
